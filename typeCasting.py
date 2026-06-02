65537da6f2aae55fef8d9366600d6c9631044bda
MadMax <lydiasakhripro@gmail.com>
2026-06-02 13:28:43 +0200

    Create typeCasting.py

---
+++ typeCasting.py
@@ -0,0 +1,14 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+
+name = "Lyds"
+age =25
+gpa = 3.2
+is_student = True
+
+age = str(age)
+print(type(age))
+
+name = bool(name)
+print(name) #True
