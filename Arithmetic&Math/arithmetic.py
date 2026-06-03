aef22492c6cb46f792fba4717d9dda70792f9303
MadMax <lydiasakhripro@gmail.com>
2026-06-03 14:57:00 +0200

    Create arithmetic.py

---
+++ Arithmetic&Math/arithmetic.py
@@ -0,0 +1,26 @@
+# Arithmetic & Math
+
+friends = 4
+friends += 1 # augmented assignment operator
+friends *= 2
+
+friends = friends ** 2 #power
+#or
+result = pow(4,3)
+remainder = friends % 3
+print(friends)
+print(remainder)
+
+z = 6
+x=3.14
+x = round(x)
+print(x)
+
+y=-4
+y = abs(y)
+print(y)
+
+result = max(x,y,z)
+print(f"The max is:{result}")
+result = min(x,y,z)
+print(f"The min is:{result}")
